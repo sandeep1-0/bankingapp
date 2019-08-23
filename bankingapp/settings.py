cd14d62c5414fa89793cbec167c418658c4d50a6
sandeep1-0 <sandeepp163@gmail.com>
2019-08-23 20:35:35 +0530

    changes in settings.py

--- bankingapp/settings.py
+++ bankingapp/settings.py
@@ -94,6 +94,11 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
